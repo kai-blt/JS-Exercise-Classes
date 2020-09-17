1757fb493a0ac1b1e703f72a34e32bc98fa5d542
kai-blt <blaitong@gmail.com>
2020-09-17 07:52:38 -0700

    completed task 5

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ class Car {
         this.milesDriven++;      
         this.odometer++;
         this.distToDrive--;
-        console.log(`Your odometer reads: ${this.odometer} and there is ${this.tank} gallons left in your tank.`);  
+        // console.log(`Your odometer reads: ${this.odometer} and there is ${this.tank} gallons left in your tank.`);  //DEBUG
         if (this.milesDriven === this.milesPerGallon) { 
             this.tank--;
             this.milesDriven = 0;    
@@ -138,7 +138,15 @@ const Bugatti = new Car('Bugatti', 10);
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
 
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}.`
+  }
 }
 
 /*
@@ -155,8 +163,21 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
+class Instructor extends Lambdasian{
+  constructor(attributes) {
+    super(attributes);
+    this.specialty = 'SQL';
+    this.favLanguage = 'C#';
+    this.catchPhrase = 'Don\'t forget the homies';
+  }
 
+  demo(subject) {
+    return `Today we are learning about ${subject}`;
+  }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
+  }
 }
 
 /*
@@ -174,8 +195,25 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(attributes) {
+    super(attributes);
+    this.previousBackground = 'Plumber';
+    this.className = 'WebEU 3';
+    this.favSubjects = [ 'JS', 'Node', 'Redux' ];
+  }
 
+  listSubjects() {
+    return `Loving ${this.favSubjects[0]}, ${this.favSubjects[1]}, ${this.favSubjects[2]}!`
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`
+  }
 }
 
 /*
